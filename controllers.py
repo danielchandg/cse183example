1ef17b085f1f792b0dd855d2b537d56348080ec2
danielchandg <dawichan@ucsc.edu>
2023-05-01 10:27:32 -0700

    may 1

--- controllers.py
+++ controllers.py
@@ -30,11 +30,59 @@ from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
 from py4web.utils.url_signer import URLSigner
 from .models import get_user_email
+import random
+import uuid
+import json
 
 url_signer = URLSigner(session)
 
 @action('index')
-@action.uses('index.html', db, auth)
+@action.uses('index.html', db, session, auth)
 def index():
     print("User:", get_user_email())
-    return dict(msg="This is my orange.")
+    c = session.get('num_visits', 0)
+    c += 1
+    session['num_visits'] = c
+    return dict(num_visits=c)
+
+@action('simple')
+@action.uses('simple.html', auth.user)
+def simple():
+    r = random.randint(0, 10)
+    return dict(r=r)
+
+# Session tracker using db
+# If no session is detected, gives the browser a session ID and a corresponding db entry
+# Files modified: controllers.py, models.py, oldie.html
+# Considerations: Multiple tabs have different session IDs.
+# Use cookies so multiple tabs share the same session ID.
+@action('oldie')
+@action.uses('oldie', db)
+def oldie():
+    # Get the session ID
+    s = request.params.get('s')
+    if s is None:
+        # Create a session ID
+        s = uuid.uuid1()
+        redirect(URL('oldie', vars=dict(s=s)))
+    
+    c = db(db.poker.secret == s).select().first()
+    if c is None:
+        d = {}
+    else:
+        d = json.loads(c.content)
+    # d is the session contents of the session ID (e.g. uuid, user, cookies, num_visits)
+    counter = d.get('counter', 0)
+    counter += 1
+    d['counter'] = counter
+    # Store d back to the database
+    if c is None:
+        db.poker.insert(secret=s, content=json.dumps(d))
+    else:
+        db(db.poker.secret == s).update(content=json.dumps(d))
+    return dict(counter=counter)
+
+@action('add', method=["GET", "POST"])
+@action.uses('add.html', url_signer, db, session, auth.user)
+def add():
+    form = Form(db.product, csrf_session=session, formstyle=FormStyleBulma)
--- models.py
+++ models.py
@@ -10,6 +10,9 @@ from pydal.validators import *
 def get_user_email():
     return auth.current_user.get('email') if auth.current_user else None
 
+def get_user_id():
+    return auth.current_user.get('id') if auth.current_user else None
+
 def get_time():
     return datetime.datetime.utcnow()
 
@@ -19,5 +22,24 @@ def get_time():
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
+
+db.define_table('poker',
+                Field('secret'),
+                Field('content', 'text'),
+                Field('number_of_days', 'integer')
+)
+
+db.define_table('pets',
+                Field('species', requires=IS_IN_SET(['Cat', 'Dog'])),
+                Field('number_of_paws', 'integer', default=4),
+                Field('description', 'text'),
+                Field('created_on', 'datetime', default=get_time),
+                Field('created_by_email', default=get_user_email), # We set default as a function
+                # If the user ID was deleted from the auth_user table, the pet in this table should not be deleted
+                Field('created_by_id', "reference auth_user", default=get_user_id, ondelete='SET NULL'),
+)
+
+db.pets.created_on.readable = False
+db.pets.created_on.writable = False
 
 db.commit()
